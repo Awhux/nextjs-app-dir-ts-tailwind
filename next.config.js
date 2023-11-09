13d04d9827c33ba9e94351da2943c6ea6ed94c12
Awhux <brudoismilequatro@gmail.com>
2023-11-09 11:04:14 -0300

    Add analyticsId, reactStrictMode, and sassOptions to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  analyticsId: process.env.NEXT_PUBLIC_ANALYTICS_ID || '',
+  reactStrictMode: true,
+  sassOptions: {
+    compress: true,
+    poweredByHeader: false,
+  },
+}
 
 module.exports = nextConfig
